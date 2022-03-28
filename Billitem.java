b9e9b9ef885e1ba9206ce8b3fc3ef06eae0d06fe
SSUDHAYARAJAN <102026028+SSUDHAYARAJAN@users.noreply.github.com>
2022-03-28 15:33:55 +0530

    Update Billitem.java

--- Billitem.java
+++ Billitem.java
@@ -22,20 +22,18 @@ public class Billitem
 	int AddtoBillItemstoDB(Connection con) throws Exception
 	{
 		ResultSet rs;
-		String query="insert into billings.billitems  (billid,itemid,storeid,itemname,itemprice,quantity,discountpercentage) values (?,?,?,?,?,?,?)";
+		String query="insert into billings.billitems  (billid,itemid,itemname,itemprice,quantity,discountpercentage) values (?,?,?,?,?,?)";
 		PreparedStatement st=con.prepareStatement(query);
 		st.setInt(1, billid);
 		st.setInt(2, itemid);
-		st.setInt(3,sid);
-		st.setString(4,itemname);
-		st.setInt(5,itemprice);
-		st.setInt(6, quantity);
-		st.setInt(7, discount);
+		st.setString(3,itemname);
+		st.setInt(4,itemprice);
+		st.setInt(5, quantity);
+		st.setInt(6, discount);
 		st.executeUpdate();
 		st.close();
 		Statement stm=con.createStatement();
 		query=String.format("update billings.storeitems set stock=stock-%d where itemid=%d and storeid=%d",quantity,itemid,sid);
-		//query=String.format("update billings.items set itemstock=itemstock-%d where itemid=%d",quantity,itemid);
 		stm.executeUpdate(query);
 		stm.close();
 		query="select price from  billitems  order by billitemid desc limit 1";
